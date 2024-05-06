8030b077d5e0603766dd97a36d3985d0b1755295
Nesmeian <lenya.kozlovskiy.97@mail.ru>
2024-05-06 23:12:17 +0300

    feat:add sorting input for countries and fix loyalt and animtion of drop-down list

--- src/components/registrationForm/countryList.ts
+++ src/components/registrationForm/countryList.ts
@@ -1,14 +1,35 @@
 import country from 'country-list-js';
 import * as baseComponent from '../../utils/baseComponent';
 import * as registrationComponent from './registrationForm';
+function searchCountry(this: HTMLInputElement): void {
+  const countyItems = document.querySelectorAll('.address__countries-item');
+  const value = this.value.toLowerCase();
+  countyItems.forEach((user) => {
+    const userText = user.textContent?.toLowerCase() || '';
+    let match = true;
+    for (let i = 0; i < value.length; i++) {
+      if (userText[i] !== value[i]) {
+        match = false;
+        break;
+      }
+    }
+    if (!match) {
+      (user as HTMLElement).style.display = 'none';
+    } else {
+      (user as HTMLElement).style.display = 'flex';
+    }
+  });
+}
 
 export function addCounties(this: HTMLElement): void {
-  const countrys = country.names();
-  const countrysList = registrationComponent.adressListCountry;
+  const countrys = country.names().sort();
+  const input = registrationComponent.adressInputCountry;
   const post: HTMLInputElement | HTMLElement =
     registrationComponent.adressInputPost;
   this.textContent = '';
   this.classList.add('--expanded');
+  input.classList.add('countrys-input--expanded');
+  input.addEventListener('input', searchCountry);
   countrys.forEach((e) => {
     const countrysItem = baseComponent.createElement({
       tag: 'div',
@@ -19,11 +40,12 @@ export function addCounties(this: HTMLElement): void {
     countrysItem.addEventListener('click', (element) => {
       element.stopPropagation();
       post.removeAttribute('disabled');
+      input.classList.remove('countrys-input--expanded');
       this.classList.remove('--expanded');
       this.textContent = countrysItem.textContent;
       this.addEventListener('click', addCounties);
     });
   });
 
-  countrysList.removeEventListener('click', addCounties);
+  this.removeEventListener('click', addCounties);
 }
