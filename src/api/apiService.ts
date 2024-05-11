0200b64843310daf45695b9991c88d0fb4b4fcaf
Ivan <dropdeaddropdead@yandex.ru>
2024-05-11 21:13:24 +0300

    feat: add refreshtokenflow, autologin function, handling navigation during logined/unauthorized sessions, implement token saving function

--- src/api/apiService.ts
+++ src/api/apiService.ts
@@ -1,20 +1,30 @@
-import { ctpClient, anonymousCtpClient } from './BuildClient';
+import {
+  ctpClient,
+  anonymousCtpClient,
+  createRefreshTokenClient,
+  createPasswordFlowClient,
+} from './BuildClient';
 import {
   createApiBuilderFromCtpClient,
   ClientResponse,
   Project,
   CustomerSignInResult,
+  Customer,
 } from '@commercetools/platform-sdk';
 import router from '../router/router';
 import { appEvents } from '../utils/eventEmitter';
 
 // apiRoot с авторизованным клиентом
+//УДАЛИТЬ СТРОКУ НИЖУ ПОСЛЕ ПЕРВОГО ЮЗА В КОДЕ
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const apiRoot = createApiBuilderFromCtpClient(ctpClient).withProjectKey({
   projectKey: import.meta.env.VITE_CTP_PROJECT_KEY,
 });
 
 // apiRoot с анонимным клиентом
-const anonymousApiRoot = createApiBuilderFromCtpClient(anonymousCtpClient).withProjectKey({
+const anonymousApiRoot = createApiBuilderFromCtpClient(
+  anonymousCtpClient,
+).withProjectKey({
   projectKey: import.meta.env.VITE_CTP_PROJECT_KEY,
 });
 
@@ -27,24 +37,51 @@ export const loginUser = async (body: {
   password: string;
 }): Promise<ClientResponse<CustomerSignInResult> | undefined | unknown> => {
   try {
-    const data = await apiRoot.login().post({ body }).execute();
+    const passFlowClient = createApiBuilderFromCtpClient(
+      createPasswordFlowClient({ email: body.email, password: body.password }),
+    ).withProjectKey({
+      projectKey: import.meta.env.VITE_CTP_PROJECT_KEY,
+    });
+    const data = await passFlowClient.login().post({ body }).execute();
     router.navigate('/');
     appEvents.emit('login', undefined);
     return data;
   } catch (error) {
-    console.error('Login failed', error);
     return error;
   }
 };
 
 export const logoutUser = async (): Promise<void> => {
   try {
-    // при логауте переключаемся на анонимный режим
+    localStorage.removeItem('token');
     const response = await anonymousApiRoot.get().execute();
     console.log('Switched to anonymous mode:', response);
-    router.navigate('/login'); // на логин
+    router.navigate('/login');
     appEvents.emit('logout', undefined);
   } catch (error) {
     console.error('Logout failed', error);
   }
 };
+
+export async function isUserLogined(): Promise<ClientResponse<Customer> | void> {
+  if (localStorage.getItem('token')) {
+    const unparsedToken = JSON.parse(localStorage.getItem('token') as string);
+    const refreshToken = unparsedToken.refreshToken;
+    const refreshFlowClient = createApiBuilderFromCtpClient(
+      createRefreshTokenClient(refreshToken),
+    ).withProjectKey({
+      projectKey: import.meta.env.VITE_CTP_PROJECT_KEY,
+    });
+    try {
+      const userData = await refreshFlowClient.me().get().execute();
+      router.navigate('/');
+      appEvents.emit('login', undefined);
+      return userData;
+    } catch (error) {
+      router.navigate('/login');
+    }
+  } else {
+    await getProject();
+    console.log('Switched to anonymous mode');
+  }
+}
