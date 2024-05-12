ca90538455b4ab184d73f3d7a5710af590a7fabb
elena-dogman <dogman.choli@gmail.com>
2024-05-12 10:44:50 +0300

    feat: update jest config

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,5 +1,12 @@
 module.exports = {
+  testEnvironment: 'jsdom',
   coverageDirectory: 'coverage',
   collectCoverage: true,
   coverageReporters: ['lcov', 'text', 'html'],
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx}',
+    '!src/**/*.spec.js',
+    '!src/**/*.test.js',
+  ],
+  testPathIgnorePatterns: ['/node_modules/'],
 };
