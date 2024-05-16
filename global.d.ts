1132e61b67adeda457df77da1d8c606893eca17d
elena-dogman <dogman.choli@gmail.com>
2024-05-16 11:49:28 +0300

    feat: create tests for year validation and correct alidation of DOB

--- global.d.ts
+++ global.d.ts
@@ -1,9 +1,23 @@
-/* eslint-disable no-var */
 declare global {
-  var window: Window & typeof globalThis;
-  var document: Document;
-  var navigator: Navigator;
-  var importMeta: {
+  interface Global {
+    window: Window & typeof globalThis;
+    document: Document;
+    navigator: Navigator;
+    importMeta: {
+      env: {
+        VITE_CTP_PROJECT_KEY: string;
+      };
+    };
+  }
+
+  namespace NodeJS {
+    interface Global extends Global {}
+  }
+
+  let window: Window & typeof globalThis;
+  let document: Document;
+  let navigator: Navigator;
+  let importMeta: {
     env: {
       VITE_CTP_PROJECT_KEY: string;
     };
--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,24 +1,27 @@
 import { beforeAll, afterAll } from 'vitest';
 import { JSDOM } from 'jsdom';
 
-// Настройка jsdom
 const dom = new JSDOM('', { url: 'http://localhost/' });
 
 global.window = dom.window as unknown as Window & typeof globalThis;
 global.document = dom.window.document;
 global.navigator = dom.window.navigator;
 
-global.importMeta = {
+(global as typeof globalThis & {
+  importMeta: {
+    env: {
+      VITE_CTP_PROJECT_KEY: string;
+    };
+  };
+}).importMeta = {
   env: {
     VITE_CTP_PROJECT_KEY: 'mock_project_key',
   },
 };
 
 beforeAll(() => {
-  // Дополнительная настройка перед всеми тестами
 });
 
 afterAll(() => {
-  // Очистка после всех тестов
   dom.window.close();
 });
