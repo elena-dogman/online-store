f2bb76ea9d281f5ed47c2a788c820a04361777b3
elena-dogman <dogman.choli@gmail.com>
2024-05-25 12:09:27 +0300

    feat: atyle filters and refactor catalog page

--- src/pages/CatalogPage/CatalogPage.ts
+++ src/pages/CatalogPage/CatalogPage.ts
@@ -1,9 +1,10 @@
 import { createElement, addInnerComponent, clear, ElementParams } from '../../utils/baseComponent';
 import { createHeader } from '../../components/header/header';
-import { createProductCatalog } from '../../components/productCatalog/productCatalog';
-import { createFilterComponent } from '../../components/productFilter/productFilter';
+import { createProductCatalog } from '../../components/catalog/productCatalog/productCatalog';
+import { createFilterComponent } from '../../components/catalog/productFilter/productFilter';
 import { fetchFilteredProducts, fetchProducts } from '../../api/apiService';
 import { ProductProjection } from '@commercetools/platform-sdk';
+import { createPagination } from '../../components/catalog/pagination/pagination';
 
 export async function createCatalogPage(): Promise<HTMLElement> {
   const pageContainerParams: ElementParams<'div'> = {
@@ -30,55 +31,12 @@ export async function createCatalogPage(): Promise<HTMLElement> {
   };
   const catalogContainer = createElement(catalogContainerParams);
 
-  const paginationContainerParams: ElementParams<'div'> = {
-    tag: 'div',
-    classNames: ['pagination'],
-  };
-  const paginationContainer = createElement(paginationContainerParams);
-
   const header = createHeader();
   const filterComponent = await createFilterComponent();
-
-  const filterTextParams: ElementParams<'p'> = {
-    tag: 'p',
-    textContent: 'Use the filters below to narrow down your product search:',
-    classNames: ['filter-instructions'],
-  };
-  const filterText = createElement(filterTextParams);
 
   let currentPage = 1;
   const itemsPerPage = 8;
   let allProducts: ProductProjection[] = await fetchProducts();
-
-  const renderPagination = (totalItems: number, itemsPerPageCount: number, currentPageIndex: number): void => {
-    const totalPages = Math.ceil(totalItems / itemsPerPageCount);
-
-    clear(paginationContainer);
-
-    const createPageButton = (pageNumber: number): HTMLElement => {
-      const pageButton = createElement({
-        tag: 'button',
-        textContent: pageNumber.toString(),
-        classNames: pageNumber === currentPageIndex ? ['active'] : [],
-        callbacks: [
-          {
-            eventType: 'click',
-            callback: (): void => {
-              currentPage = pageNumber;
-              renderProducts(allProducts, currentPage, itemsPerPageCount);
-            },
-          },
-        ],
-      });
-
-      return pageButton;
-    };
-
-    for (let i = 1; i <= totalPages; i++) {
-      const pageButton = createPageButton(i);
-      addInnerComponent(paginationContainer, pageButton);
-    }
-  };
 
   const renderProducts = (products: ProductProjection[], page: number, itemsPerPageCount: number): void => {
     clear(catalogContainer);
@@ -90,7 +48,19 @@ export async function createCatalogPage(): Promise<HTMLElement> {
     const productCatalog = createProductCatalog(paginatedProducts);
     addInnerComponent(catalogContainer, productCatalog);
 
-    renderPagination(products.length, itemsPerPageCount, page);
+    const pagination = createPagination({
+      totalItems: products.length,
+      itemsPerPage: itemsPerPageCount,
+      currentPage: page,
+      onPageChange: (newPage) => {
+        currentPage = newPage;
+        renderProducts(products, currentPage, itemsPerPageCount);
+      },
+    });
+
+    clear(catalogContainerWrapper);
+    addInnerComponent(catalogContainerWrapper, catalogContainer);
+    addInnerComponent(catalogContainerWrapper, pagination);
   };
 
   filterComponent.addEventListener('change', async () => {
@@ -123,11 +93,9 @@ export async function createCatalogPage(): Promise<HTMLElement> {
 
   pageContainer.prepend(header);
   addInnerComponent(pageContainer, filterWrapper);
-  addInnerComponent(filterWrapper, filterText);
   addInnerComponent(filterWrapper, filterComponent);
   addInnerComponent(pageContainer, catalogContainerWrapper);
-  addInnerComponent(catalogContainerWrapper, catalogContainer);
-  addInnerComponent(catalogContainerWrapper, paginationContainer);
+
   renderProducts(allProducts, currentPage, itemsPerPage);
 
   return pageContainer;
