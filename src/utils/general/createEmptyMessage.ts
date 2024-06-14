a9a3a7d1219c93d7033ca97a937ca5dbb8988335
Nesmeian <lenya.kozlovskiy.97@mail.ru>
2024-06-14 18:07:51 +0300

    feat:add link to catalog page

--- src/utils/general/createEmptyMessage.ts
+++ src/utils/general/createEmptyMessage.ts
@@ -1,10 +1,24 @@
-import { createElement } from './baseComponent';
+import { addInnerComponent, createElement } from './baseComponent';
 
 export function createEmptyMessage(): HTMLElement {
-  const emptyMessage = createElement({
+  const emptyMessageLink = createElement({
+    tag: 'a',
+    classNames: ['basket-products__empty-message', 'empty-message__link'],
+    textContent: 'Go to catalog?',
+    attributes: { href: '/catalog' },
+  });
+  const emptyMessageText = createElement({
     tag: 'p',
     classNames: ['basket-products__empty-message'],
     textContent: 'Your cart is empty.',
   });
+  const lineBreak = createElement({ tag: 'br' });
+  const emptyMessage = createElement({
+    tag: 'div',
+    classNames: ['empty-message'],
+  });
+  addInnerComponent(emptyMessage, emptyMessageText);
+  addInnerComponent(emptyMessage, lineBreak);
+  addInnerComponent(emptyMessage, emptyMessageLink);
   return emptyMessage;
 }
